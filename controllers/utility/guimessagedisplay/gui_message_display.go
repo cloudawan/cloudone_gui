15ddd1f04c6b44a66a33f3e0844d82cc2fd3293e
edcyc <edcyc.tw@gmail.com>
2015-08-28 11:08:12 +0800

    Put the apache 2 license into code

--- controllers/utility/guimessagedisplay/gui_message_display.go
+++ controllers/utility/guimessagedisplay/gui_message_display.go
@@ -1,13 +1,26 @@
+// Copyright 2015 CloudAwan LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package guimessagedisplay
 
-import (
-)
+import ()
 
 type GUIMessage struct {
 	successSlice []string
-	infoSlice []string
+	infoSlice    []string
 	warningSlice []string
-	dangerSlice []string
+	dangerSlice  []string
 }
 
 const (
@@ -52,7 +65,7 @@ type SessionUtility interface {
 	GetSession(name interface{}) interface{}
 
 	// SetSession removes value from session.
-	DelSession(name interface{}) 
+	DelSession(name interface{})
 }
 
 func (guiMessage *GUIMessage) RedirectMessage(sessionUtility SessionUtility) {
@@ -86,4 +99,3 @@ func (guiMessage *GUIMessage) OutputMessage(data map[interface{}]interface{}) bo
 		return true
 	}
 }
-
